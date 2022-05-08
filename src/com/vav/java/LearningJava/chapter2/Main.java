a0e8cf1c2ff5cb49c32c6edcfb2b10b419048649
AndreyVasylyuk <AndreyVasylyuk@gmail.com>
2022-05-08 16:42:09 +0300

    initial project commit

---
+++ src/com/vav/java/LearningJava/chapter2/Main.java
@@ -0,0 +1,7 @@
+package com.vav.java.LearningJava.chapter2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Learning Java - Loy, Neimeyer, Leuck, 5th ed, 2020");
+    }
+}
